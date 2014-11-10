8cc5db823463cdcb03c792c8fa13593fb46b80a5
cmigayi <migayicecil@gmail.com>
2014-11-10 19:44:06 +0300

    static page about the company file

---
+++ about.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Goods clearance club | customs clearance service</title>
+<link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+<div class="wrapper">
+	
+	<header>
+		<div id="logo"><img src="img/logo.png"/></div>
+		<div id="navbar">
+			<ul>
+				<li><a href="index.php"><img src="img/png/home63.png" id="icons"/>&nbsp;HOME</a></li>
+				<li><a href="about.php" id="current_link"><img src="img/png/creative4.png" id="icons"/>&nbsp;WHAT IS <span style="color:#f8323b;">GCC</span>?</a></li>
+				<li><a href="contactus.php"><img src="img/png/telephone38.png" id="icons"/>&nbsp;CONTACT US</a></li>
+			</ul>
+		</div>
+		<div id="enquiry">
+			<a href="">Make Quick Enquiry</a>
+		</div>
+		<div class="clear"></div>
+	</header>
+	
+	<div class="main_content">
+		<div id="link_name">
+			<img src="img/png/creative4.png" />
+			<h3>WHAT IS <span style="color:#f8323b;">GCC</span>?</h3>
+		</div>
+		<div id="link_info">
+			<h4>GCC:</h4>
+			<p>GCC is "Goods Clearance Club" in full.</p>	
+			<h4>Purpose:</h4>
+			<p>GCC  brings together several importers, mainly SME and individuals by providing a chance to leverage their group purchasing power for affordable brokerage fees..</p>	
+			<h4>Advantage:</h4>
+			<p>The club invites bids from  Licensed Customs Brokers. The Winning bidder qualify as a preferred broker to serve the club for one year
+			 unless otherwise stated. All The club members get to use the same account number. Through the Club,our members access the most competitive 
+			 Customs brokerage rates.</p>	
+		</div>
+		<div class="clear"></div>
+	</div>
+	
+	<footer>
+		<div>&copy; Goods Clearance Club 2014</div>
+		<div>Developed and maintained by <a href="http://trascope.com/" target="_blank">trascope solutions</a></div>		
+	</footer>
+	
+</div>
+
+</body>
+
+</html>
