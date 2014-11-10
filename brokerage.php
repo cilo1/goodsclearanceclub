493de4c2d548953b420633f909a68b31d0719416
cmigayi <migayicecil@gmail.com>
2014-11-10 21:01:45 +0300

    brokers static information

---
+++ brokerage.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Goods clearance club | customs clearance service</title>
+<link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+<div class="wrapper">
+	
+	<header>
+		<div id="logo"><img src="img/logo.png"/></div>
+		<div id="navbar">
+			<ul>
+				<li><a href="index.php"><img src="img/png/home63.png" id="icons"/>&nbsp;HOME</a></li>
+				<li><a href="about.php"><img src="img/png/creative4.png" id="icons"/>&nbsp;WHAT IS <span style="color:#f8323b;">GCC</span>?</a></li>
+				<li><a href="contactus.php"><img src="img/png/telephone38.png" id="icons"/>&nbsp;CONTACT US</a></li>
+			</ul>
+		</div>
+		<div id="enquiry">
+			<a href="enquiry.php">Make Quick Enquiry</a>
+		</div>
+		<div class="clear"></div>
+	</header>
+	
+	<div class="main_content">
+		<div id="mini_menu">
+			<ul>
+				<li id="how1"> <a href="howitworks.php">How Goods Clearance club works? how brokers compete for business?&raquo;</a></li>
+				<li id="member1"> <a href="freemembershipinfo.php">Free Membership &raquo;</a></li>
+				<li id="brokery1"><a href="brokerage.php"> Brokerage bids & accounts &raquo;</a></li>			
+				<li id="import1"><a href="importservices.php"> Import Services &raquo;</a></li>			
+				<li id="group1"><a href="gcc_conception.php"> GCC Conception &raquo;</a></li>
+			</ul>
+			
+		</div>
+		<div id="broker_det">
+			<h2>Brokerage bids & accounts</h2>
+			<div id="broker_info">
+			<p>
+				The Brokers compete for the group  business ( club members),  
+				once one firm is selected to serve the Club, the normal bidding circle  is open in twelve months, unless otherwise stated.
+			</p>
+			<p> All customs brokers have equal chance to bid, they have to meet our requirements- fully    
+			registered with Customs Agency, fully bonded, up to date, experienced, flexible to offer  
+			competitive rates to our members and address all concerns promptly and satisfactorily. 
+			The terms are twelve months, the Club reserves all rights to amend terms with notice.  Read our <a href="">terms and conditions</a></p>
+			<p>Click links below to signup or login as broker.</p>
+			</div>
+			<div id="broker_links"><a href="broker_login.php">Login as broker</a></div><div id="broker_links"><a href="broker_signup.php">Signup as broker</a></div>
+		</div>
+		<div class="clear"></div>
+	</div>
+	
+	<footer>
+		<div>&copy; Goods Clearance Club 2014</div>
+		<div>Developed and maintained by <a href="http://trascope.com/" target="_blank">trascope solutions</a></div>		
+	</footer>
+	
+</div>
+
+</body>
+
+</html>
